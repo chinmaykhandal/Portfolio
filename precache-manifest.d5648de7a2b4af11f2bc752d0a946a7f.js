79805c5897d4271383437d7d2adf8f2b62ec85a1
chinmaykhandal <khandalchinmay@gmail.com>
2022-09-25 12:18:40 +0530

    Updates

---
+++ precache-manifest.d5648de7a2b4af11f2bc752d0a946a7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63f512a2384536864f354778b7082853",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "8ca0d4d9aac3cf93f10c",
+    "url": "/Portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "4cdf00155ef777914280",
+    "url": "/Portfolio/static/css/main.406feb11.chunk.css"
+  },
+  {
+    "revision": "8ca0d4d9aac3cf93f10c",
+    "url": "/Portfolio/static/js/2.86c1525b.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Portfolio/static/js/2.86c1525b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cdf00155ef777914280",
+    "url": "/Portfolio/static/js/main.fed760aa.chunk.js"
+  },
+  {
+    "revision": "c125469937b460e6de34",
+    "url": "/Portfolio/static/js/runtime-main.3379861c.js"
+  }
+]);
